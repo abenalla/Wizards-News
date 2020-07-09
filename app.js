3955f4770845d67e18972c4e8274a3afd4a10580
unknown <aishaben.18@gmail.com>
2020-07-09 17:21:55 -0400

    commit

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+//First, get the list of posts.
+const posts = postBank.list();
+
+//Then, prepare some html to send as output.
+const html = `<! DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `<li>${/*You have access to the past data here*/}</li>`)}
+    <ul>
+  </body>
+  </html>`;
+
+// Finally, send a response
+res.send(html);
+
+});
 
 const PORT = 1337;
 
